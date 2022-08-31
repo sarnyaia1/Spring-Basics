794642beffd21a9f3fb274b2d30bf3ea0308f8de
Akos_Sarnyai <Akos_Sarnyai@epam.com>
2022-08-31 10:10:29 +0200

    Add Spring Database to the project

---
+++ src/main/java/com/epam/spring/learningspring/data/Room.java
@@ -0,0 +1,36 @@
+package com.epam.spring.learningspring.data;
+
+import lombok.Data;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name="ROOM")
+@Data
+public class Room {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+
+    @Column(name = "ROOM_ID")
+    private long id;
+
+    @Column(name = "NAME")
+    private String name;
+
+    @Column(name = "ROOM_NUMBER")
+    private String roomNumber;
+
+    @Column(name = "BED_INFO")
+    private String bedInfo;
+
+    @Override
+    public String toString() {
+        return "Room{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", roomNumber='" + roomNumber + '\'' +
+                ", bedInfo='" + bedInfo + '\'' +
+                '}';
+    }
+}
---
+++ src/main/java/com/epam/spring/learningspring/util/AppStartupEvent.java
@@ -0,0 +1,23 @@
+package com.epam.spring.learningspring.util;
+
+import com.epam.spring.learningspring.data.Room;
+import com.epam.spring.learningspring.data.RoomRepository;
+import org.springframework.boot.context.event.ApplicationReadyEvent;
+import org.springframework.context.ApplicationListener;
+import org.springframework.stereotype.Component;
+
+@Component
+public class AppStartupEvent implements ApplicationListener<ApplicationReadyEvent> {
+
+    private final RoomRepository roomRepository;
+
+    public AppStartupEvent(RoomRepository roomRepository){
+        this.roomRepository = roomRepository;
+    }
+
+    @Override
+    public void onApplicationEvent(ApplicationReadyEvent event) {
+        Iterable<Room> rooms = this.roomRepository.findAll();
+        rooms.forEach(System.out::println);
+    }
+}
